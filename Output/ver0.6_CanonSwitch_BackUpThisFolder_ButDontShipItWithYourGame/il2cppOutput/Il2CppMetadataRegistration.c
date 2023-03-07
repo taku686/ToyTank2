293e29b85c7c1c22fb4ee69aa6761076bf38e2d1
DESKTOP-G5L353K\takun <takunoshin123456789@gmail.com>
2023-03-07 15:07:51 +0900

    ver0.6のビルドを生成。

---
+++ Output/ver0.6_CanonSwitch_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,41 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+IL2CPP_EXTERN_C_CONST int32_t* g_FieldOffsetTable[];
+IL2CPP_EXTERN_C_CONST Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	26405,
+	g_Il2CppGenericTypes,
+	21192,
+	g_Il2CppGenericInstTable,
+	91488,
+	g_Il2CppGenericMethodFunctions,
+	58920,
+	g_Il2CppTypeTable,
+	121303,
+	g_Il2CppMethodSpecTable,
+	14849,
+	g_FieldOffsetTable,
+	14849,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
